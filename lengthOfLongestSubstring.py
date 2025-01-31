ed530655a389f4f638e197ba13964a2e863329ec
umesh2302 <65848324+umesh2302@users.noreply.github.com>
2025-01-31 10:16:58 +0530

    day 02
    problem 3

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,11 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        dicts = {} # create a dictionary
-        substring = "" # create a substring
+        temp_string = "" # store the current substring
+        longest_string = "" # store the longest substring found so far
         for i in range(len(s)):
-            if substring == "":
-                substring = substring + s[i]
+            if s[i] in temp_string:
+                if len(temp_string) > len(longest_string):
+                    longest_string = temp_string
+                    
+                temp_string = temp_string[temp_string.index(s[i])+1:] + s[i]
             else:
-                if s[i] != 
+                temp_string = temp_string + s[i]
+        
+        if len(temp_string) > len(longest_string):
+            longest_string = temp_string
 
-            dicts
+        return len(longest_string)
+    
+# test cases
+test = Solution().lengthOfLongestSubstring("vdvev") # 
+print(test) # 3
